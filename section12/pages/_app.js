523315c5e6dafba6eed9daa95550fe0e9c934b96
HYHYJ <jjangdaram@naver.com>
2024-03-13 10:38:31 +0900

    📝 13-3 전체 레이아웃 설정하기

--- section12/pages/_app.js
+++ section12/pages/_app.js
@@ -1,5 +1,13 @@
+//모든 페이지의 부모역할
 import "@/styles/globals.css";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
 }

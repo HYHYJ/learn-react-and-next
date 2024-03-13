cab9ef177c19b7f7b63d9f9df52f387110b4cf47
HYHYJ <jjangdaram@naver.com>
2024-03-13 09:55:31 +0900

    📝 13-1. 페이지 라우터 실습

---
+++ section12/pages/country/[code].js
@@ -0,0 +1,9 @@
+//라우터의 모든 기능이 다들어있다.
+import { useRouter } from "next/router";
+
+export default function Country() {
+  const router = useRouter();
+  //url 코드 값 가져오기
+  const { code } = router.query;
+  return <div>Country {code}</div>;
+}

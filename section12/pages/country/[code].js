3adb7d22e84fbc23a1b4294587bfae7a34ef8a3e
HYHYJ <jjangdaram@naver.com>
2024-03-14 11:10:26 +0900

    📝 14-2 SSR 적용하기(2)

--- section12/pages/country/[code].js
+++ section12/pages/country/[code].js
@@ -1,12 +1,33 @@
 //라우터의 모든 기능이 다들어있다.
+import { fetchCountry } from "@/api";
 import SubLayout from "@/components/SubLayout";
 import { useRouter } from "next/router";
 
-export default function Country() {
+export default function Country({ country }) {
+  //리액트 훅, 수화과정이 끝나고 동작, 클라이언트에서 이용할려면 컴포넌트 내부에서 불러오기
   const router = useRouter();
-  //url 코드 값 가져오기
+  //
   const { code } = router.query;
-  return <div>Country {code}</div>;
+  //url 코드 값 가져오기
+
+  return (
+    <div>
+      {country.commonName} {country.officialName}
+    </div>
+  );
 }
 
 Country.Layout = SubLayout;
+
+//? context: url 정보를 가져오는 context 객체
+export const getServerSideProps = async (context) => {
+  // 서버측에서 데이터를 패치하고싶으면 context에서 가져오면 된다.
+  const { code } = context.params;
+  let country = null;
+  if (code) {
+    country = await fetchCountry(code);
+  }
+  return {
+    props: { country },
+  };
+};
--- section12/pages/index.js
+++ section12/pages/index.js
@@ -1,17 +1,23 @@
-export default function Home({ name }) {
-  console.log("Home");
-  return <div>{name}</div>;
+import { fetchCountries } from "@/api";
+
+export default function Home({ countries }) {
+  return (
+    <div>
+      {countries.map((country) => (
+        <div key={country.code}>{country.commonName}</div>
+      ))}
+    </div>
+  );
 }
 //SSR 방식으로 된다
 export const getServerSideProps = async () => {
-  // SSR을 위해 서버측에서 페이지 컴포넌트에게 전달할 데이터를 설정하는 함수
+  // API 호출
+  const countries = await fetchCountries();
 
-  //서버에서만 실행되서 브라우저의 console에 안나옴
-  console.log("getServerSideProps Called");
   return {
     //?무조건 객체로 설정하기
     props: {
-      name: "KOREA",
+      countries,
     },
   };
 };
